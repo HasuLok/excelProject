363fd7f35679c1259ebf297e44d6793b4879fc58
lok <hasuteam@mail.com>
2022-01-03 17:19:49 +0300

    finished base table logic

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,12 +3,27 @@ const CODES = {
     Z: 90
 }
 
-function toCell(_, col) {
-    return `
-        <div class="cell"
-         contenteditable spellcheck="false"
-          data-col="${col}"></div>
-    `
+// function toCell(_, col) {
+//     return `
+//         <div class="cell"
+//          contenteditable spellcheck="false"
+//           data-col="${col}"></div>
+//     `
+// }
+function toCell(row) {
+    return function(_, col) {
+            return `
+                <div 
+                class="cell"
+                contenteditable 
+                spellcheck="false"
+                data-type="cell"
+                data-col="${col}"
+                data-row="${row}"
+                data-id="${row}:${col}"
+                ></div>
+            `
+    }
 }
 
 function toColumn(col, index) {
@@ -49,12 +64,12 @@ export function createTable(rowsCount = 15) {
 
     rows.push(createRow(null, cols))
 
-    for (let i = 0; i < rowsCount; i++) {
+    for (let row = 0; row < rowsCount; row++) {
         const cells = new Array(colsCount)
         .fill('')
-        .map(toCell)
+        .map(toCell(row))
         .join('');
-        rows.push(createRow(i + 1, cells));
+        rows.push(createRow(row + 1, cells));
     }
 
     return rows.join('');
